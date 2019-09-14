05308b6417ecba29534d38f5eb72ce1b40a0ef4a
muzzamil75 <muzamil.ijaz11@gmail.com>
2019-09-14 17:14:28 +0500

    delpyoin to NOW and netlify

--- client/src/index.js
+++ client/src/index.js
@@ -7,11 +7,8 @@ import * as serviceWorker from "./serviceWorker";
 export const BASE_URL =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:4000"
-    : "https://node-lambda.reed-barger.now.sh";
+    : "https://node-lambda.muzzamil75.now.sh/";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
